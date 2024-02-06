556c7d8abc65d39ccdb47bf11c7f26949fb27dd5
UsmanDrigrocha <UsmanDrigrocha@Gmail.com>
2024-02-06 17:34:50 +0500

    created getter

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,11 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-// import {
-//   ActivatedRouteSnapshot,
-//   Router,
-//   RouterStateSnapshot,
-//   UrlTree,
-// } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -13,28 +7,23 @@ import { Observable } from 'rxjs';
 })
 export class AuthService {
   private apiBaseUrl = 'http://localhost:3001/api/';
-  private _isAuthenticated: boolean = false;
 
   constructor(private http: HttpClient) {
-    this.checkAuthentication();
   }
 
-  private checkAuthentication(): void {
-    const token = localStorage.getItem('token');
-    this._isAuthenticated = !!token;
-  }
-
-  login(email: string, password: string): any {
+  login(email: string, password: string): Observable<any> {
     return this.http.post(this.apiBaseUrl + 'auth/login', { email, password });
   }
 
   logout(): void {
-    this._isAuthenticated = false;
+    localStorage.removeItem('token');
   }
 
   isAuthenticated(): boolean {
-    const token = localStorage.getItem('token');
-    this._isAuthenticated = !!token;
-    return this._isAuthenticated;
+    return !!localStorage.getItem('token');
+  }
+
+  get isAuthenticatedStatus(): boolean {
+    return this.isAuthenticated();
   }
 }
