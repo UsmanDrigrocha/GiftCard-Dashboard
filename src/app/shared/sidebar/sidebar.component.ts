c184ef1e32874c2422f494e5e9fbd8e0ff4240cf
UsmanDrigrocha <UsmanDrigrocha@Gmail.com>
2024-02-12 10:53:19 +0500

    sidebar working ok

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -3,18 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   logout() {
     localStorage.removeItem('token');
-    window.location.reload()
+    window.location.reload();
   }
 
+  isSidebarHidden: boolean = false;
+
+  
+  toggleSidebar() {
+    this.isSidebarHidden = !this.isSidebarHidden;
+  }
 }
